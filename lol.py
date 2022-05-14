080937d5b86ee795aedc452d775bfcafad71c8da
levon <airtk@ya.ru>
2022-05-14 10:47:28 +0400

    та кроче сделеи обрезание ифов им не завидую

--- lol.py
+++ lol.py
@@ -6,6 +6,7 @@ wrap.world.create_world(1000,700)
 wrap.add_sprite_dir("vechaslavtop")
 
 pervey = wrap.sprite.add("daed",500,500, "kvadrat_1")
+# pervey = wrap.sprite.add("mario-1-small",500,500, "climb2")
 
 @wrap.always()
 def polet_pryamougolnik_1 ():
@@ -13,7 +14,7 @@ def polet_pryamougolnik_1 ():
     bottom=wrap.sprite.get_bottom(pervey)
 
     if bottom<695 :
-        wrap.sprite.move(pervey, 0, +5)
+        wrap.sprite.move(pervey, 0, +1)
         bottom = wrap.sprite.get_bottom(pervey)
     gradus = wrap.sprite.get_angle(pervey)
     if  gradus==90 :
@@ -22,7 +23,7 @@ def polet_pryamougolnik_1 ():
 
 
 
-@wrap.on_mouse_down(wrap.BUTTON_LEFT)
+@wrap.on_mouse_down(wrap.BUTTON_LEFT )
 def povorot_perve ():
 
     gradus=wrap.sprite.get_angle(pervey)
@@ -32,6 +33,44 @@ def povorot_perve ():
     if bottom>695:
         gradus-=90
         wrap.sprite.set_angle(pervey, gradus)
+
+@wrap.on_mouse_down( wrap.BUTTON_RIGHT)
+def povorot_pravo():
+    gradus = wrap.sprite.get_angle(pervey)
+    gradus-=90
+    wrap.sprite.set_angle(pervey,gradus)
+    bottom=wrap.sprite.get_bottom(pervey)
+    if bottom>695:
+        gradus += 90
+        wrap.sprite.set_angle(pervey, gradus)
+
+
+
+@wrap.always()
+def povorot_pravo(keys):
+    x = wrap.sprite.get_x(pervey)
+    y = wrap.sprite.get_y(pervey)
+
+    if wrap.K_a in keys:
+        x-=10
+
+    if wrap.K_d in keys:
+        x+=10
+    wrap.sprite.move_to(pervey,x,y)
+
+
+
+
+
+
+        # x=wrap.sprite.get_x(pervey)
+        # x-=10
+        # y=wrap.sprite.get_y(pervey)
+        #
+        # wrap.sprite.move_to(pervey,x,y)
+
+
+
 
 
 # сначала ми его поварачиваем  +90
@@ -46,6 +85,10 @@ def povorot_perve ():
 #
 #     gradus+=90
 #     wrap.sprite.set_angle(pervey, gradus)
+
+
+
+
 
 
 
