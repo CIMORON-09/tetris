6a84ed2d3a5ce3e534654b8c2eae50a64ac25248
levon <airtk@ya.ru>
2022-05-15 14:05:00 +0400

    та кроче сделеи обрезание ифов им не завидую

--- lol.py
+++ lol.py
@@ -1,3 +1,5 @@
+import random
+
 import wrap
 
 
@@ -5,21 +7,40 @@ wrap.world.create_world(1000,700)
 
 wrap.add_sprite_dir("vechaslavtop")
 
-pervey = wrap.sprite.add("daed",500,500, "kvadrat_1")
+pervey=None
+
 # pervey = wrap.sprite.add("mario-1-small",500,500, "climb2")
+
+def randon_kartinka():
+    global pervey
+    what=random.randint(1,4)
+    if what==1:
+        pervey = wrap.sprite.add("daed",500,-200, "kvadrat_1")
+    if what==2:
+        pervey = wrap.sprite.add("daed", 500,-200, "kvadrat_2")
+    if what==3:
+        pervey = wrap.sprite.add("daed", 500, -200, "kvadrat_3")
+    if what==4:
+        pervey = wrap.sprite.add("daed", 500, -200, "kvadrat_4")
+
 
 @wrap.always()
 def polet_pryamougolnik_1 ():
     global pervey
+
+    if pervey== None:
+        randon_kartinka()
+
     bottom=wrap.sprite.get_bottom(pervey)
 
     if bottom<695 :
-        wrap.sprite.move(pervey, 0, +1)
+        wrap.sprite.move(pervey, 0, +50)
         bottom = wrap.sprite.get_bottom(pervey)
     gradus = wrap.sprite.get_angle(pervey)
     if  gradus==90 :
-        if bottom>695:
+        if bottom>700:
             wrap.sprite.move_to(pervey,500,668)
+
 
 
 
@@ -30,7 +51,7 @@ def povorot_perve ():
     gradus+=90
     wrap.sprite.set_angle(pervey,gradus)
     bottom=wrap.sprite.get_bottom(pervey)
-    if bottom>695:
+    if bottom>700:
         gradus-=90
         wrap.sprite.set_angle(pervey, gradus)
 
@@ -40,7 +61,7 @@ def povorot_pravo():
     gradus-=90
     wrap.sprite.set_angle(pervey,gradus)
     bottom=wrap.sprite.get_bottom(pervey)
-    if bottom>695:
+    if bottom>700:
         gradus += 90
         wrap.sprite.set_angle(pervey, gradus)
 
@@ -48,45 +69,24 @@ def povorot_pravo():
 
 @wrap.always()
 def povorot_pravo(keys):
+    if pervey==None:
+        return
     x = wrap.sprite.get_x(pervey)
     y = wrap.sprite.get_y(pervey)
+    bottom=wrap.sprite.get_bottom(pervey)
+    if  bottom<699 :
+        if wrap.K_a in keys:
+            x-=10
 
-    if wrap.K_a in keys:
-        x-=10
-
-    if wrap.K_d in keys:
-        x+=10
-    wrap.sprite.move_to(pervey,x,y)
-
-
-
-
-
-
-        # x=wrap.sprite.get_x(pervey)
-        # x-=10
-        # y=wrap.sprite.get_y(pervey)
-        #
-        # wrap.sprite.move_to(pervey,x,y)
-
-
-
+        if wrap.K_d in keys:
+            x+=10
+        wrap.sprite.move_to(pervey,x,y)
 
 
 # сначала ми его поварачиваем  +90
 # проверяем боттом
 # проверяяем зошла ли он за граници экрана
 # если она зошла то тогда возращаем обратно
-
-
-# @wrap.on_mouse_down(wrap.BUTTON_RIGHT)
-# def povorot_perve():
-#     gradus = wrap.sprite.get_angle(pervey)
-#
-#     gradus+=90
-#     wrap.sprite.set_angle(pervey, gradus)
-
-
 
 
 
